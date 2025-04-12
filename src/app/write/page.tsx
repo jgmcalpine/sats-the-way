67bb3901316fc1bc7855533c99c0fb9e049839d2
Jon <j.g.mcalpine@gmail.com>
2025-04-12 07:50:19 -0700

    Stripped down home and write pages

---
+++ src/app/write/page.tsx
@@ -0,0 +1,7 @@
+export default function WritePage() {
+    return (
+        <div className="flex justify-center items-center h-full min-h-screen">
+            This is where we will write
+        </div>
+    )
+}
