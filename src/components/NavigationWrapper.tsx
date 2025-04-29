4e0e46e4717b571b479ebeb60ec42161f4dfaf84
Jon <j.g.mcalpine@gmail.com>
2025-04-28 19:20:00 -0700

    beep

--- src/components/NavigationWrapper.tsx
+++ src/components/NavigationWrapper.tsx
@@ -4,11 +4,11 @@ import { useAuth } from "@/components/AuthProvider";
 import TopNav from "@/components/TopNav";
 
 export default function NavigationWrapper() {
-  const { currentUser, connect, loading, disconnect } = useAuth();
+  const {  loading } = useAuth();
 
   if (loading) {
     console.log("show loading");
     return null;
   }
-  return <TopNav onDisconnect={disconnect} currentUser={currentUser} onConnect={connect} loading={loading} />
+  return <TopNav  />
 }
