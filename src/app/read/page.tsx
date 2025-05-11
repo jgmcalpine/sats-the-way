3e34f281b7541d76c1255e85ea561b5638d6719c
Jon <j.g.mcalpine@gmail.com>
2025-05-10 22:28:09 -0700

    beep

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { useState , useEffect} from "react";
 import { useRouter } from 'next/navigation';
 
-import { CircularProgress, Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 import BookGrid from '@/components/ui/BookGrid';
 
