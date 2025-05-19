d97287641e5ffbd674776c2193edcc909e0f6c72
Jon <j.g.mcalpine@gmail.com>
2025-05-18 21:02:18 -0700

    About updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,11 @@ export default function AboutPage() {
     <LayoutWrapper>
       <Box className="flex flex-col w-full">
         <Box className="flex flex-col gap-8">
-          <Typography className="grow flex justify-center items-center" variant="h3">
+          <Typography
+            color="primary"
+            className="grow flex justify-center items-center"
+            variant="h3"
+          >
             SatsTheWay
           </Typography>
           <Typography variant="h6">
@@ -16,25 +20,37 @@ export default function AboutPage() {
             bitcoin payments and openly available to any nostr client.
           </Typography>
           <Typography variant="body1">
-            Neither bitcoin or nostr are required to read free content on SatsTheWay. To fully
-            experience the application, one can connect a nostr account (to publish a story) and/or
-            a lightning wallet (to pay for stories that implement paywalls).
+            Neither bitcoin or nostr are required to{' '}
+            <Link className="text-blue-600" href="/read">
+              read
+            </Link>{' '}
+            free content on SatsTheWay. To fully experience the application, one can connect a nostr
+            account (to publish a story) and lightning wallet (to pay for stories that implement
+            paywalls and receive payments for your work).
           </Typography>
           <Typography variant="body1">
-            As always do your own research. We have used{' '}
+            As always do your own research. For very quick lightning setup we have used{' '}
             <Link className="text-blue-600" href="https://www.coinos.io">
               coinos.io
             </Link>{' '}
-            and found it very simple to set up. If you are just testing the application or dipping
-            your toes into lightning with small amounts, this is a simple way to experience it.
+            and found it very user friendly. If you are just testing the application or dipping your
+            toes into lightning with small amounts, this is a simple way to experience it.
           </Typography>
           <Typography variant="body1">
             To publish a story, a nostr account must be connected. This is so we can create events
             (books and chapters). In order to connect, please install a browser extension that works
             as a nip-07 signer. DYOR rigby,{' '}
-            <Link href="https://github.com/fiatjaf/nos2x">Nos2x</Link> and{' '}
-            <Link href="https://github.com/getAlby/lightning-browser-extension">Alby</Link> have
-            worked for us.
+            <Link className="text-blue-600" href="https://github.com/fiatjaf/nos2x">
+              Nos2x
+            </Link>{' '}
+            and{' '}
+            <Link
+              className="text-blue-600"
+              href="https://github.com/getAlby/lightning-browser-extension"
+            >
+              Alby
+            </Link>{' '}
+            have worked for us.
           </Typography>
           <Typography variant="body1">
             Finally, if you publish a story and would like to paywall certain paths, you will need
